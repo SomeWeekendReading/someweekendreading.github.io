88038f0609bcc37c6cafd093df870dc00836fe70
Weekend Editor <SomeWeekendReadingEditor@gmail.com>
2021-08-28 09:38:51 -0400

    Tweak

--- assets/2021-08-29-covid-simpson-ve-by-age.r
+++ assets/2021-08-29-covid-simpson-ve-by-age.r
@@ -14,20 +14,20 @@ doit <- function(cohorts    = c("12-15", "16-19", "20-29", "30-39", "40-49", "50
                  efficacies = c(100.0,   100.0,   100.0,   96.8,    93.9,    92.8,
                                  88.7,    89.6,    81.1,   92.4),
                  plotFile   = "./2021-08-29-covid-simpson-ve-by-age.png") {
-  withPNG(plotFile, 400, 400, FALSE, function() {      #
-    withPars(function() {                              #
-      plot(x = 1 : length(cohorts), y = efficacies,    #
+  withPNG(plotFile, 400, 400, FALSE, function() {      # Capture graphics to file
+    withPars(function() {                              # Save/restore graphics params
+      plot(x = 1 : length(cohorts), y = efficacies,    # Plot points & lines
            type = "b", pch = 21, col = "black", bg = "blue",
            xaxt = "n", xlab = NA, ylab = "Vaccine Efficacy (%)", ylim = c(0, 100),
            main = "Vaccine efficacy by age cohort")    # Horizontal axis done manually
       axis(side = 1, at = 1 : length(cohorts), labels = cohorts)
       title(xlab = "Age Cohort", line = 3.5)           # Horizontal axis title
-    }, pty = "m",                                      #
-       bg  = "white",                                  #
-       mar = c(5, 3, 2, 1),                            #
-       mgp = c(1.7, 0.5, 0),                           #
-       ps  = 16,                                       #
-       las = 2)                                        #
-  })                                                   #
+    }, pty = "m",                                      # Maximal plotting area
+       bg  = "white",                                  # White background
+       mar = c(5, 3, 2, 1),                            # Margins for axis labels, title
+       mgp = c(1.7, 0.5, 0),                           # Axis title, labels, ticks
+       ps  = 16,                                       # Larger type size for file capture
+       las = 2)                                        # Axis labels always perp to axis
+  })                                                   # Done!
 }                                                      #
 
