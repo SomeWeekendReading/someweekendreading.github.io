d24f3c377d8650ee6ef1b0fa0b0b0e8016dd8fc1
Weekend Editor <SomeWeekendReadingEditor@gmail.com>
2021-11-27 11:22:48 -0500

    Tweak

--- post-stats.r
+++ post-stats.r
@@ -11,26 +11,45 @@ library("RCurl")                                       # For getURLContent()
 ## Collect stats on posts
 ##
 
-postHits <- function(postsDir = "./_posts",
+postHits <- function(## Inputs
+                     postsDir = "./_posts",
                      mdRegexp = "^([0-9]{4}-[0-9]{2}-[0-9]{2})-(.*)\\.md$",
                      countNS  = "www.someweekendreading.blog",
                      countURL = sprintf("https://api.countapi.xyz/get/%s", countNS),
                      baseURL  = sprintf("https://%s", countNS)
-                     ## *** Add option to save in ./_drafts/post-hit-counts-%d.tsv (put in date)
-                     ) {
+
+                     ## Outputs
+                     destFile = sprintf("./drafts/post-hit-counts-%s.tsv", Sys.Date())) {
+
+  ## What the analytics page does:
+  ##
+  ## https://api.countapi.xyz/get/www.someweekendreading.blog/{{ page.url | escape | replace: "/", "." }}
+  ##
+  ## NB: page.url returns a string like "/name/", so this escapes and puts periods
+  ## both before and after: ".name.".  Historical accident, but let's stick with it.
 
   ## *** Add pipeline stuff, transcript, etc.  (Trnascript to file similar to .tsv save file)
-  ddply(data.frame(PostFile = list.files(path = postsDir, pattern = "*.md")), "PostFile",
-        function(postFile) {                           #
-          pf            <- as.character(postFile[1, 1])#
-          postDate      <- as.Date(sub(mdRegexp, "\\1", pf))
-          ## What the analytics page does:
-          ## https://api.countapi.xyz/get/www.someweekendreading.blog/{{ page.url | escape | replace: "/", "." }}
-          postCountURL  <- sprintf("%s/%s",            #
-                                   countURL, gsub("/", "\\.", URLencode(sub(mdRegexp, "/\\2/", pf))))
-          postCountJSON <- getURLContent(postCountURL) #
-          data.frame(PostDate  = postDate,             #
-                     PostCount = as.integer(sub("^\\{.*\"value\":([0-9]+)\\}$", "\\1",
-                                                postCountJSON)))
-        }, .progress = progress_text())                #
+  ans <- ddply(data.frame(PostFile = list.files(path = postsDir, pattern = "*.md")), "PostFile",
+               function(postFile) {                    # Process each post's .md file
+                 pf            <- as.character(postFile[1, 1])
+                 postDate      <- as.Date(sub(mdRegexp, "\\1", pf))
+                 postCount     <- as.integer(sub("^\\{.*\"value\":([0-9]+)\\}$",
+                                                 "\\1",#
+                                                 getURLContent(sprintf("%s/%s",
+                                                                       countURL,
+                                                                       gsub("/",
+                                                                            "\\.",
+                                                                            URLencode(sub(mdRegexp,
+                                                                                          "/\\2/",
+                                                                                          pf)))))))
+                 data.frame(PostDate = postDate, PostCount = postCount)
+               }, .progress = progress_text())         # Takes a minute or so; show progress
+
+  if (!is.null(destFile)) {                            # If specified destination file
+    saveDataframe(ans, destFile)                       #   then save results
+    cat(sprintf("\nSaved to %s.", destFile))           #
+  }                                                    #
+
+  ans                                                  # Return the dataframe of results
 }                                                      #
+
