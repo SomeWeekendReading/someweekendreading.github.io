b04126443cb831835af30f9a285f9c14d2dae6e3
Weekend Editor <SomeWeekendReadingEditor@gmail.com>
2021-12-26 16:54:02 -0500

    Tweak

--- assets/post-stats.r
+++ assets/post-stats.r
@@ -109,6 +109,9 @@ postStats <- function(## Inputs
                  HitsEnd      = today)                 #  and today.  Counts are in that interval.
     }, .progress = progress_text())                    # Takes a minute; might as well show progress
 
+    ## *** Number of posts broken down by year
+    ## cat(sprintf("\n\n* Breakdown of post counts by year:\n"))
+
     ## *** Might it be better to do this in the ldply(), to avoid having to collect all the rest?
     if (!is.na(year)) {                                # Wants to restrict to a single year
       stopifnot(is.integer(year)                                &&
