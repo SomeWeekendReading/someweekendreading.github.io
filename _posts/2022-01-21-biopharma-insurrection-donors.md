35c16151b5ff9010c7c03ffb4c43d551b78c725e
Weekend Editor <SomeWeekendReadingEditor@gmail.com>
2022-01-21 15:50:20 -0500

    New post

---
+++ _posts/2022-01-21-biopharma-insurrection-donors.md
@@ -0,0 +1,166 @@
+---
+layout: post
+title: Shameful BioPharma Donations to Insurrectionists
+tags: PharmaAndBiotech Politics R
+comments: true
+---
+
+Regrettably, it has come to light that my former industry has some bad actors who are
+funding the Republican politicians who attempted to overthrow the last election.  Who are
+these funders of insurrectionists, and once we know their companies can we shame them into
+stopping?  
+
+## A shame for the US biopharma sector  
+
+<img src="{{ site.baseurl }}/images/2022-01-21-biopharma-insurrection-donors-lowe.jpg" width="400" height="283" alt="Derek Lowe: Shame on biopharma donors to insurrectionists" title="Derek Lowe: Shame on biopharma donors to insurrectionists" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;">
+<img src="{{ site.baseurl }}/images/2022-01-21-biopharma-insurrection-donors-usdin.jpg" width="400" height="254" alt="Usdin @ BioCentury: BioPharmas contributing to pols who voted to overthrow election" title="Usdin @ BioCentury: BioPharmas contributing to pols who voted to overthrow election" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;">
+<img src="{{ site.baseurl }}/images/2022-01-21-biopharma-insurrection-donors-capaction.jpg" width="400" height="248" alt="CAP Action: Corporations funding McCarthy's insurrection caucus" title="CAP Action: Corporations funding McCarthy's insurrection caucus" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;">
+Sometime last week, I came across an article by the estimable med-chem blogger Derek Lowe
+at _In the Pipeline_, title simply "Shame". <sup id="fn1a">[[1]](#fn1)</sup>  True enough,
+he discusses an article from Steve Usdin in _BioCentury_ <sup id="fn2a">[[2]](#fn2)</sup> 
+on donations by biopharma companies to US representatives (and 1 senator) who tried _to
+overthrow the US government_ last year, by overruling the election results.  If you think
+that's unique to biopharma, consult the article in CAP Action from mid-2021 documenting
+how much of corporate America is funding insurrectionists. <sup id="fn3a">[[3]](#fn3)</sup>  
+
+It's hard to describe just how much this angers me.  BioPharma scientists are among the
+most ethical people I've met in my life. <sup id="fn4a">[[4]](#fn4)</sup>  It's espcially
+galling since many of them pledged not to donate to any members of Congress who voted to
+decertify the election&hellip; but within a few months, the 
+[dog had returned to the vomit](https://en.wikipedia.org/wiki/As_a_dog_returns_to_his_vomit,_so_a_fool_repeats_his_folly#:~:text=%22As%20a%20dog%20returns%20to,New%20Testament%2C%202%20Peter%202%3A).  
+
+Derek apparently feels the same way.  It should be noted that as Derek is well to the
+right of me, he and I agree on approximately nothing in politics.  Nothing, that is,
+except the execrable nature of Trump and all but a very few Republicans.  
+
+From the insurrection riot on 2021-Jan-06 through 2021-Nov-03, 9 biopharmas and their
+trade group BIO contributed to 42 lawmakers who voted for decertification of the
+election.  (PhRMA, another trade group, has adopted a policy of not donating to candidates
+who reject election results.  As far as anyone can tell, they have stuck to it.)  
+
+Here are the donors, ranked by the number of insurrectionists they funded:  
+
+| Donor     | | NRecipients |
+|:---------:|-|------------:|
+| Pfizer    | |          24 |
+| Merck     | |          19 |
+| Lilly     | |          15 |
+| Amgen     | |          13 |
+| J&amp;J   | |          11 |
+| GSK       | |           9 |
+| BIO       | |           8 |
+| Genentech | |           5 |
+| Novartis  | |           5 |
+| AbbVie    | |           4 |
+
+Clearly the top offenders are Pfizer, Merck, Lilly, Amgen, and J&amp;J.  I'm _slightly_
+gratified to note the absence of any of my former employers in this list of the damnable.
+(However, another article -- reference 3 -- implies that one of them _did_ contribute to
+the [NRCC](https://www.nrcc.org/) after promising to do no such thing.  See below re
+sociopaths in management; perhaps they prefer sociopaths in government as well.)  
+
+Note that this list is not just for Republican contributions, odious as that may be.
+While these organizations have also donated to Democrats, here they have made
+contributions _specifically to politicians who tried to overthrow the election._  It's
+really that bad.  
+
+Who are those politicians?  Here are the top few of the recipients, who got donations from
+more than 5 biopharma donors:  
+
+|            Recipient | | NDonors |
+|---------------------:|-|--------:|
+|      Rep Mullin (OK) | |      9  |
+|       Rep Smith (NB) | |      8  |
+|   Rep Arrington (TX) | |      7  |
+|    Rep Walorski (IN) | |      7  |
+|      Rep Carter (GA) | |      6  |
+|       Rep Kelly (PA) | |      5  |
+|    Rep McCarthy (CA) | |      5  |
+|       Rep Nunes (CA) | |      5  |
+|     Rep Scalise (LA) | |      5  |
+|       Rep Smith (MT) | |      5  |
+
+Needless to say, they are all Republicans.  
+
+It's too bad we don't have the _amount_ of the donations as well.  It would be good to add
+up the dollars and assess the influence of money, not just number of donors.  
+
+Shame, indeed.  
+
+
+## What kind of structure can we infer from the donation data?  
+
+<img src="{{ site.baseurl }}/assets/2022-01-21-biopharma-insurrection-donors.png" width="400" height="1000" alt="Bipartite graph structure of donors and insurrection politicians" title="Bipartite graph structure of donors and insurrection politicians" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;">
+If we think of the donor/recipient data as a 
+[bipartite graph](https://en.wikipedia.org/wiki/Bipartite_graph) (links from donors to
+recipients, but never donor-donor or recipient-recipient links), then we can begin to see
+some structure.  (Indeed, beyond the outrage, this is the one original contribution I'm
+making with this article.)  We took the figure from Usdin's article, marshalled the data into a
+tab-separated spreadheet, and wrote a little [R script](https://www.r-project.org/) to
+plot it as a bipartite graph <sup id="fn5a">[[5]](#fn5)</sup>, shown here.  
+
+A few points become blunt-trauma-obvious:  
+- The main biopharma offenders are Pfizer, Merck, Lilly, Amgen, and J&amp;J.  The others
+  are there, but at least the donated to fewer insurrectionists.  This accords with what
+  we saw in the table above.  
+- The biggest offending insurrectionist politicians in terms of the number of funding
+  sources accepted were Mullin, Smith, Walorski, Arrington, Carter, Smith, Scalise, Nunes,
+  McCarthy.  This also accords with what we saw in the table above.  
+
+
+## The Weekend Conclusion  
+
+I dunno.  
+
+I'd dearly love to see us attempt to apply the 14th Amendment to the US
+Constitution, adopted after the Civil War, which says in part:  
+
+> Section 3  
+>  
+> No person shall be a Senator or Representative in Congress, or elector of President and
+> Vice-President, or hold any office, civil or military, under the United States, or under
+> any State, who, having previously taken an oath, as a member of Congress, or as an
+> officer of the United States, or as a member of any State legislature, or as an
+> executive or judicial officer of any State, to support the Constitution of the United
+> States, shall have engaged in insurrection or rebellion against the same, or given aid
+> or comfort to the enemies thereof.  
+
+Why are these guys still members of Congress?  
+
+It seems like the right wing is so amazingly full of crap, they have to export it onto the
+rest of us.  
+
+What can be done here?  Any ideas?  
+
+
+---
+
+## Notes &amp; References  
+
+<!--
+<sup id="fn1a">[[1]](#fn1)</sup>
+
+<a id="fn1">1</a>: ***, ["***"](***), *** [↩](#fn1a)  
+
+<a href="{{ site.baseurl }}/images/***"><img src="{{ site.baseurl }}/images/***" width="400" height="***" alt="***" title="***" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;"></a>
+
+<iframe width="400" height="224" src="***" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;"></iframe>
+-->
+
+<a id="fn1">1</a>: D Lowe, ["Shame"](https://www.science.org/content/blog-post/shame), [_In the Pipeline_](https://www.science.org/blogs/pipeline) blog at _Science Translational Medicine_, 2022-Jan-13. [↩](#fn1a)  
+
+<a id="fn2">2</a>: S Usdin, ["Biopharmas contributing to candidates who challenged Biden’s election"](https://www.biocentury.com/article/641697), _BioCentury_, 2022-Jan-11. [↩](#fn2a)  
+
+<a id="fn3">3</a>: _CAP Action_, ["The corporations trying to save Kevin McCarthy and the insurrection caucus"](https://capaction.medium.com/the-corporations-trying-to-save-kevin-mccarthy-and-the-insurrection-caucus-43a74488416c), _CAP Action_, 2021-Jun-03. [↩](#fn3a)  
+
+<a id="fn4">4</a>: Though I grant that management has its share of sociopaths and sales will always try to do something shady if they're not watched.  
+
+Hence every year we all had to take the usual inane HR training that the things you think are obviously crimes, are in fact crimes.  This was all because some idiot tried to bump his sales numbers, got caught, found guilty, drummed out of the industry, and left the rest of us with 10 years of annual training.  
+
+This made scientists _furious._ [↩](#fn4a)  
+
+<a id="fn5">5</a>: [WeekendEditor](mailto:SomeWeekendReadingEditor@gmail.com), ["R script to show bipartite graph structure of donors and insurrectionist politicians"]({{ site.baseurl }}/assets/2022-01-21-biopharma-insurrection-donors.r), [_Some WeekendReading_]({{ site.baseurl }}) blog, 2022-Jan-21.  
+
+We have aggregated the figures in Lowe and Usdin's articles, collecting the donors and recipients into [a tab-separated spreadsheet which is available for download and review]({{ site.baseurl }}/assets/2022-01-21-biopharma-insurrection-donors.tsv).  
+
+Also, after running the R script, there is [a text transcript available for download and review]({{ site.baseurl }}/assets/2022-01-21-biopharma-insurrection-donors.txt). [↩](#fn5a)  
---
+++ assets/2022-01-21-biopharma-insurrection-donors.r
@@ -0,0 +1,219 @@
+## Created on Wednesday, January 19, 2022 at 5:45pm EST by WeekendEditor on WeekendEditorMachine.
+## Copyright (c) 2022, SomeWeekendReading.blog.  All rights reserved.  As if you care.
+
+suppressPackageStartupMessages({                       # Ssshh!  Quiet in the library...
+  toolsDir <- "../../tools"                            # Tools available from the author
+  source(file.path(toolsDir, "pipeline-tools.r"))      # Pipeline construction tools
+  source(file.path(toolsDir, "graphics-tools.r"))      # Various graphics tools
+
+  library("igraph")                                    # Finite graphs
+  library("plyr")                                      # For ddply()
+})                                                     #
+
+##
+## Biopharma corporate donations do US reps & senators who supported the 2021-Jan-06 insurrection.
+## https://www.biocentury.com/article/641697
+## https://www.science.org/content/blog-post/shame
+## https://capaction.medium.com/the-corporations-trying-to-save-kevin-mccarthy-and-the-insurrection-caucus-43a74488416c
+##
+
+doit <- function(## Inputs
+                 dataDir  = ".",                       #
+                 dataFile = "2022-01-21-biopharma-insurrection-donors.tsv",
+                 ## Shorten names of donors
+                 donorMap = c("Biotechnology Innovation Organization (BIO)" = "BIO",
+                              "Genentech unit of Roche"                     = "Genentech",
+                              "Merck & Co."                                 = "Merck",
+                              "Johnson & Johnson"                           = "J&J",
+                              "Eli Lilly"                                   = "Lilly",
+                              "GlaxoSmithKline"                             = "GSK"),
+                 ## Shorten names of states of recipients
+                 stateMap = c("Texas" = "TX", "Kan." = "KS", "Okla." = "OK", "Calif." = "CA",
+                              "Neb." = "NB", "Pa." = "PA", "Ala." = "AL", "Ind." = "IN", "Ill." = "IL",
+                              "Ga." = "GA", "Ariz." = "AZ", "Wyo." = "WY", "S.C." = "SC", "La." = "LA",
+                              "Mont." = "MT", "Fla." = "FL", "N.C." = "NC", "Ohio" = "OH", "Va." = "VA",
+                              "N.Y." = "NY"),
+
+                 ## Outputs
+                 destDir  = ".",                       #
+                 txFile   = sub("^(.*)\\.tsv$", "\\1.txt", dataFile),
+                 plotFile = sub("^(.*)\\.tsv$", "\\1.png", dataFile)) {
+
+  loadData <- function(dataDir, dataFile, donorMap, stateMap) {
+    f <- file.path(dataDir, dataFile)                  # Where teh data lives
+    cat(sprintf("* Loading data from %s", f))          # Yack about it
+
+    donorData <- read.table(f, sep = "\t", header = TRUE)
+    cat(sprintf("\n  - Found %d columns (%s) x %d rows:\n",
+                ncol(donorData), paste(colnames(donorData), collapse = ", "), nrow(donorData)))
+    stopifnot(ncol(donorData) == 2 && colnames(donorData) == c("Donor", "Recipient"))
+
+    ## Shorten recipient names:
+    recipRegex <- "^(.*)\\. (.*), .* \\((.*)\\)$"      # Regexp to capture recip title, surname, state
+    titles     <- sub(recipRegex, "\\1", as.character(donorData$"Recipient"))
+    surnames   <- sub(recipRegex, "\\2", as.character(donorData$"Recipient"))
+    states     <- stateMap[sub(recipRegex, "\\3", as.character(donorData$"Recipient"))]
+    donorData  <- transform(donorData,                 # Replace name with shorter version
+                            Recipient = factor(sprintf("%s %s (%s)", titles, surnames, states)))
+
+    ## Shorten donor names:
+    donorData <- transform(donorData,                  # If name is in map, use that else original
+                           Donor = factor(ifelse(as.character(Donor) %in% names(donorMap),
+                                                 donorMap[as.character(Donor)],
+                                                 as.character(Donor))))
+    showDataframeHeadTail(donorData)                   # Show off the fish we caught
+
+    cat(sprintf("\n  - Number of recipients for each biopharma donor:\n"))
+    foo <- ddply(donorData, "Donor", function(df) { data.frame(NRecipients = nrow(df)) })
+    foo <- foo[order(foo$"NRecipients", decreasing = TRUE), ]
+    print(foo)                                         # Show frequent donors
+
+    cat(sprintf("\n  - Number of donors for each insurrectionist recipient:\n"))
+    foo <- ddply(donorData, "Recipient", function(df) { data.frame(NDonors = nrow(df)) })
+    foo <- foo[order(foo$"NDonors", decreasing = TRUE), ]
+    print(foo)                                         # Show frequent recipients
+
+    donorData                                          # Return the dataframe
+  }                                                    #
+
+  makeDonorGraph <- function(donorData) {              # Make a bipartite, undirected graph
+    cat(sprintf("* Making graph with %d donors and %d recipients",
+                length(levels(donorData$"Donor")), length(levels(donorData$"Recipient"))))
+    donorGraph <- graph_from_incidence_matrix(table(donorData), directed = FALSE, mode = "out")
+    stopifnot(is.bipartite(donorGraph))                # Ensure it's bipartite
+    cat(sprintf("\n  - Graph is verified to be bipartite\n"))
+    donorGraph                                         # Return the graph
+  }                                                    #
+
+  plotBipartiteGraph <- function(donorGraph, donorData, destDir, plotFile) {
+    ## *** Ok, I could never make this look pretty enough to satsify me, so I
+    ##     perpeatrated the really hacky kludge that follows.
+    ## Modeled after 2nd answer in:
+    ##   https://stackoverflow.com/questions/31366066/how-to-plot-a-bipartite-graph-in-r
+    ## V(g)$shape <- shape[as.numeric(V(g)$type) + 1]
+    ## V(g)$color <- c('red', 'blue', 'green', 'steelblue', 'steelblue', 'steelblue', 'steelblue')
+    ## E(g)$color <- ## 'gray'
+    ## E(g)$color[E(g)['A' %--% V(g)]] <- 'red'
+    ## E(g)$color[E(g)['B' %--% V(g)]] <- 'blue'
+    ## E(g)$color[E(g)['C' %--% V(g)]] <- 'green'
+
+    sortedDonors <- ddply(donorData, "Donor", function(df) { data.frame(NRecipients = nrow(df)) })
+    sortedDonors <- as.character(
+        sortedDonors[order(sortedDonors$"NRecipients", decreasing = FALSE), ]$"Donor")
+
+    sortedRecips <- ddply(donorData, "Recipient", function(df) { data.frame(NDonors = nrow(df)) })
+    sortedRecips <- as.character(
+        sortedRecips[order(sortedRecips$"NDonors", decreasing = FALSE), ]$"Recipient")
+
+    ## Default xlim is [-1, +1], so put donors @ -0.5 and recipients @ +0.5
+    V(donorGraph)$"x" <- ifelse(!V(donorGraph)$"type", -0.25, +0.25)
+    ## Default ylim is [-1, +1].
+    ## - So we want the bottom vertex at y1 = -1 and the top one at y2 = +1.
+    ## - The vertex rank ranges from x1 = 1 to x2 = n.
+    ## - Slope: m = (y2 - y1) / (x2 - x1) = 2 / (n - 1).
+    ## - Slope-point formula for linear embedding:
+    ##   y - y1 = m * (x - x1)
+    ##   y = y1 + (2 / (n-1)) * (x - 1) = y1 + 2 * (x-1) / (n-1)
+    nDonors <- length(levels(donorData$"Donor"))
+    nRecips <- length(levels(donorData$"Recipient"))
+    V(donorGraph)$"y" <- sapply(1 : length(V(donorGraph)), function(n) {
+      ## *** More vertical spacing between vertices somehow?
+      v <- V(donorGraph)[[n]]                          # sapply(V(donorGraph), ...) gets indices not vrtxs
+      if (!v$"type")
+        -1 + 2 * (match(v$"name", sortedDonors) - 1) / (nDonors - 1)
+      else
+        -1 + 2 * (match(v$"name", sortedRecips) - 1) / (nRecips - 1)
+    })
+    V(donorGraph)$"color"        <- ifelse(!V(donorGraph)$"type", "lightgray", "pink")
+    V(donorGraph)$"shape"        <- ifelse(!V(donorGraph)$"type", "square",    "circle")
+    V(donorGraph)$"label.degree" <- ifelse(!V(donorGraph)$"type", pi,          0)
+    V(donorGraph)$"label.dist"   <- ifelse(!V(donorGraph)$"type",
+                                           0.5 * nchar(V(donorGraph)$"name"),
+                                           4)
+    ##V(donorGraph)$"size"  <- sapply(1 : length(V(donorGraph)), function(n) {
+    ##  7 * nchar(V(donorGraph)[[n]]$"name")
+    ##})
+    ## V(donorGraph)$"size2"  <- sapply(1 : length(V(donorGraph)), function(n) { 10 })
+
+    f <- file.path(destDir, plotFile)
+    withPNG(f, 400, 1000, FALSE, function() {
+      withPars(function() {
+        plot(donorGraph, vertex.label.color = "black", asp = FALSE)
+        ## box(which = "plot")
+      }, pty = "m",
+         bg  = "white",
+         mar = c(0, 2, 0, 5)
+      )
+    })
+  }
+
+  plotBipartiteGraph2 <- function(donorData, destDir, plotFile,
+                                  donorx = -0.65, recipx = +0.25) {
+    ## Really hacky kludge, doing it the hard way, because I don't understand igraph well enough!
+
+    ## Default ylim is [-1, +1].
+    ## - So we want the bottom vertex at y1 = -1 and the top one at y2 = +1.
+    ## - The vertex rank ranges from x1 = 1 to x2 = n.
+    ## - Slope: m = (y2 - y1) / (x2 - x1) = 2 / (n - 1).
+    ## - Slope-point formula for linear embedding:
+    ##   y - y1 = m * (x - x1)
+    ##   y = y1 + (2 / (n-1)) * (x - 1) = y1 + 2 * (x-1) / (n-1)
+    sortedDonors <- ddply(donorData, "Donor", function(df) { data.frame(NRecipients = nrow(df)) })
+    sortedDonors <- transform(transform(sortedDonors[order(sortedDonors$"NRecipients",
+                                                           decreasing = FALSE), ],
+                                        Rank = 1 : nrow(sortedDonors)),
+                              x = donorx,              # Add in points where edge joins on
+                              y = -1 + 2 * (Rank - 1) / (nrow(sortedDonors) - 1))
+    rownames(sortedDonors) <- as.character(sortedDonors$"Donor")
+
+    sortedRecips <- ddply(donorData, "Recipient", function(df) { data.frame(NDonors = nrow(df)) })
+    sortedRecips <- transform(transform(sortedRecips[order(sortedRecips$"NDonors", decreasing = FALSE), ],
+                                        Rank = 1 : nrow(sortedRecips)),
+                              x = recipx,              # Add in points where edge joins on
+                              y = -1 + 2 * (Rank - 1) / (nrow(sortedRecips) - 1))
+    rownames(sortedRecips) <- as.character(sortedRecips$"Recipient")
+
+    edges <- transform(donorData,                      # Add edge data
+                       fromx = donorx,                 # Edge goes fromhere
+                       fromy = sortedDonors[as.character(Donor),     "y"],
+                       tox   = recipx,                 #  to here
+                       toy   = sortedRecips[as.character(Recipient), "y"])
+
+    f <- file.path(destDir, plotFile)                  # Where the plot goes
+    withPNG(f, 400, 1000, FALSE, function() {          # Capture to file
+      withPars(function() {                            # Save/restore graphics params
+        plot(x = NA, y = NA, xlim = c(-1, +1), ylim = c(-1, +1),
+             axes = FALSE, ann = FALSE, frame.plot = FALSE)
+        text(x = donorx, y = sortedDonors$"y", labels = sortedDonors$"Donor",     adj = c(1, 0.5))
+        text(x = recipx, y = sortedRecips$"y", labels = sortedRecips$"Recipient", adj = c(0, 0.5))
+        ddply(edges, c("Donor", "Recipient"), function(drdf) {
+          donor <- as.character(drdf$"Donor")          # Donor name
+          recip <- as.character(drdf$"Recipient")      # Recipient name
+          lines(x = c(donorx, recipx), y = c(sortedDonors[donor, "y"], sortedRecips[recip, "y"]))
+          NULL                                         # Don't build a nonsense result
+        })                                             # Done with edges
+      }, pty = "m",                                    # Maximal plotting area
+         bg  = "white",                                # White background
+         ps  = 16,                                     # Larger type size for file capture
+         mar = c(0, 0, 0, 0)                           # Pull in on margins
+      )                                                #
+    })                                                 #
+    cat(sprintf("Plotted to %s\n", f))                 # Log what we did to transcript
+  }                                                    #
+
+  withTranscript(dataDir, destDir, txFile, "BioPharma Insurrectionist Donors", function() {
+
+    heraldPhase("Loading biopharma/insurrectionist politician donor data")
+    maybeAssign("donorData", function() { loadData(dataDir, dataFile, donorMap, stateMap) })
+
+    heraldPhase("Making bipartite graph")              # Make finite graph
+    maybeAssign("donorGraph", function() { makeDonorGraph(donorData) })
+
+    heraldPhase("Plotting graph")                      # Plot the graph
+    maybeAssign("plotDone", function() {
+      ## plotBipartiteGraph(donorGraph, donorData, destDir, plotFile)
+      plotBipartiteGraph2(donorData, destDir, plotFile)# Do it the hard way, which is easier!
+    })                                                 #
+
+  })                                                   #
+}                                                      #
