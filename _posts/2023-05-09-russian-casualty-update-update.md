727ed9a335994a0d73ed6397748724d42e550ede
Weekend Editor <SomeWeekendReadingEditor@gmail.com>
2023-05-09 13:51:26 -0400

    New post

---
+++ _posts/2023-05-09-russian-casualty-update-update.md
@@ -0,0 +1,187 @@
+---
+layout: post
+title: Updated Update&colon; Ukrainian Estimates of Russian Casualties
+tags: MathInTheNews R Sadness Statistics
+comments: true
+commentsClosed: true
+---
+
+We've (again) updated our estimate of when Russian casualties will reach 200k, according
+to the Ukrainian Ministry of Defence’s published data.  This time with an improved (though
+not perfect) prediction method.  
+
+
+## Ukrainian Ministry of Defence and Russian Casualty Counts  
+
+Background: the Ukrainian Ministry of Defence publishes daily estimates of various sorts
+of Russian casualties.  Cursory investigation shows they are higher than the OSInt numbers
+from Oryx (where they demand documentation of everything, a high standard in war) but
+lower than other media sources.  So&hellip; not really verifiable, but not the most
+extreme numbers, either.  
+
+We've previously written <sup id="fn1a">[[1]](#fn1)</sup> <sup id="fn2a">[[2]](#fn2)</sup>
+about collecting these data, looking at odd time patterns in cruise missile attacks that
+probably say something about Russian supply chains, and building regression models to
+project future casualty numbers.  
+
+Today, 2023-May-09, was the upper confidence limit on the date we thought Russian losses
+of soldiers would reach 200,000.  Let's check in to see what's going on.  
+
+
+## Data and Methods  
+
+
+We've updated the data as of today's report (2023-May-09), and updated the
+[R](https://www.r-project.org/) script to do a slightly better prediction of 200k 
+day; all are available here for peer review. <sup id="fn3a">[[3]](#fn3)</sup>
+As previously observed, the 2023-Apr-30 data is missing.  
+## Results  
+
+<a href="{{ site.baseurl }}/images/2023-05-09-russian-casualty-data-update-update-regress-Soldiers-on-DayNum.png"><img src="{{ site.baseurl }}/images/2023-05-09-russian-casualty-data-update-update-regress-Soldiers-on-DayNum-thumb.jpg" width="400" height="400" alt="UKR MoD data on Russian casualties: Soldiers vs DayNum" title="UKR MoD data on Russian casualties: Soldiers vs DayNum" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;"></a>
+For the most part, things look pretty similar to the way they looked last time, with just
+a continuation of trends.  
+
+Shown here, for example, is the trend of number of Russian soldiers killed (as counted by
+the UKR MoD, of course) versus the day number since 2023-Jan-22.  It's pretty much a
+continuation of the trend, with quite tight confidence intervals and prediction
+intervals.  The fact that a linear model fits well is a bit surprising, perhaps indicative
+of a relatively constant, grinding level of war.   
+
+The fit is indeed excellent:  
+
+```
+Coefficients:
+             Estimate Std. Error t value Pr(>|t|)    
+(Intercept) 1.220e+05  3.149e+02   387.5   <2e-16 ***
+x           7.123e+02  5.099e+00   139.7   <2e-16 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 1613 on 104 degrees of freedom
+Multiple R-squared:  0.9947,	Adjusted R-squared:  0.9946 
+F-statistic: 1.951e+04 on 1 and 104 DF,  p-value: < 2.2e-16
+```
+
+<a href="{{ site.baseurl }}/images/2023-05-09-russian-casualty-data-update-update-FvsLar7aMAAATCN.jpg"><img src="{{ site.baseurl }}/images/2023-05-09-russian-casualty-data-update-update-FvsLar7aMAAATCN-thumb.jpg" width="400" height="653" alt="UKR MoD data on Russian casualties: 2023-May-09 report" title="UKR MoD data on Russian casualties: 2023-May-09 report" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;"></a>
+One feature of the plot above possibly worth noting is that the data points appear to bend
+downward, with a slower death rate on about day 60.  The linear fit is approximately an
+average of the previous high slope and the new lower slope.  That means any prediction
+here will include influence from the early high slope, and hence _underestimate_ time time
+to 200k dead.  
+
+Indeed, that's what we observe: we were sure today would be The Very Bad Day, but it is
+not.  Today's figure was 195,620.  
+
+We could, of course, implement a nonlinear model to address this.  E.g., a piecewise
+linear model with a hinge at day 60 would do the trick, or we could actually let the hinge
+date be a fit parameter.  That would lead to a 4-parameter model: 2 slopes, 2 intercepts,
+1 hinge date, but 1 constraint that the lines meet exactly at the hinge date.  
+
+However, that would be a more complex model, rather ill-motivated by anything other than a
+desire to fit the data better when we already have an excellent fit.  So let's just live
+with the caution that our estimates will be _underestimates_, and move on.  
+
+<a href="{{ site.baseurl }}/images/2023-05-09-russian-casualty-data-update-update-regress-DayNum-on-Soldiers.png"><img src="{{ site.baseurl }}/images/2023-05-09-russian-casualty-data-update-update-regress-DayNum-on-Soldiers-thumb.jpg" width="400" height="400" alt="UKR MoD data on Russian casualties: DayNum on Soldiers" title="UKR MoD data on Russian casualties: DayNum on Soldiers" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;"></a>
+Now on to predictions!  
+
+Last time, we used the regression model of Soldiers as a function of DayNum, and
+back-solved to find the date when the number of casualties was about 200k.  That's&hellip;
+awkward, especially since the 95% confidence intervals don't work linearly like that.  
+
+So this time we'll regress DayNum on Soldiers, and use the number of losses at 200k to
+directly predict the day number and its 95% confidence limit.  The fit is shown here.
+(Don't spend too much time staring at it, since it's just the transpose of the fit above.)  
+
+The vertical dashed gray line is 200k soldiers lost.  As you can see, things are not yet
+that bad.
+
+The fit is, of course, equally as excellent as the fit above:  
+
+```
+Coefficients:
+              Estimate Std. Error t value Pr(>|t|)    
+(Intercept) -1.701e+02  1.617e+00  -105.2   <2e-16 ***
+Soldiers     1.396e-03  9.998e-06   139.7   <2e-16 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 2.258 on 104 degrees of freedom
+Multiple R-squared:  0.9947,	Adjusted R-squared:  0.9946 
+F-statistic: 1.951e+04 on 1 and 104 DF,  p-value: < 2.2e-16
+```
+
+When we use the linear model predictor to guess when casualties reach 200k, and what the
+95% lower and upper confidence limits are, we get:  
+
+```
+         fit        lwr        upr
+1 2023-05-11 2023-05-06 2023-05-15
+```
+
+Three things are worth noting here:  
+
+1. The most likely date, using data up through today, is 2023-May-11.  For the reasons
+   discussed above, the bend in slope at day 60 means this is probably a slight
+   underestimate and the real event will be a little bit after that.  
+2. The 95% confidence limit is from 2023-May-06 to 2023-May-15.  So any time from May 11
+   to May 15 is probably a decent estimate to use.  
+3. The lower confidence limit, 2023-May-06, is _in the past_.  We know with 100%
+   confidence that the date will be in the future, so this is nonsense!  We're using an
+   acausal (unaware of time and cause) linear model to predict a causal (time series,
+   where things happen in a certain order) to predict.  This is, at some level, wrong.
+   Probably if I were to take this seriously, I should go get out my old copy of Box,
+   Jenkins, and Reinsel <sup id="fn1a">[[1]](#fn1)</sup> (or even something more modern
+   like Hyndman and Athanasopoulos <sup id="fn5a">[[5]](#fn5)</sup>) and read about time
+   series forecasting.  Both of them are sitting on my shelf, but
+   [_Vīta brevis, ars longa, occāsiō praeceps, experīmentum perīculōsum, iūdicium difficile_](htttps://en.wikipedia.org/wiki/Ars_longa,_vita_brevis),
+   as Hippocrates is supposed to have said (ok, in Greek, but I only know it in Latin).  
+
+
+## The Weekend Conclusion  
+
+Using slightly (though not completely) better methods, we estimate 200k Russian casualties
+sometime in 2023-May-11 to 2023-May-15.  These dates are likely (slight) underestimates,
+i.e., the true date will likely be a bit later.  
+
+---
+
+## Notes &amp; References  
+
+<!--
+<sup id="fn1a">[[1]](#fn1)</sup>
+
+<a id="fn1">1</a>: ***, ["***"](***), *** [↩](#fn1a)  
+
+<a href="{{ site.baseurl }}/images/***">
+  <img src="{{ site.baseurl }}/images/***" width="400" height="***" alt="***" title="***" style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;">
+</a>
+
+<a href="***">
+  <img src="{{ site.baseurl }}/images/***" width="550" height="***" alt="***" title="***" style="margin: 3px 3px 3px 3px; border: 1px solid #000000;">
+</a>
+
+<iframe width="400" height="224" src="***" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="float: right; margin: 3px 3px 3px 3px; border: 1px solid #000000;"></iframe>
+-->
+
+<a id="fn1">1</a>: [Weekend Editor](mailto:SomeWeekendReadingEditor@gmail.com), ["Do the Ukrainian Reports of Russian Casualties Make Sense?"]({{ site.baseurl }}/russian-casualty-data/), [_Some Weekend Reading_ blog]({{ site.baseurl }}/), 2023-Apr-15. [↩](#fn1a)  
+
+<a id="fn2">2</a>: [Weekend Editor](mailto:SomeWeekendReadingEditor@gmail.com), ["Update: Ukrainian Estimates of Russian Casualties"]({{ site.baseurl }}/russian-casualty-data-update/), [_Some Weekend Reading_ blog]({{ site.baseurl }}/), 2023-May-01. [↩](#fn2a)  
+
+<a id="fn3">3</a>: [Weekend Editor](mailto:SomeWeekendReadingEditor@gmail.com), ["Updated R script to analyze Ukrainian reports of Russian casualties"]({{ site.baseurl }}/assets/2023-05-09-russian-casualty-update-update-ukr-rus-casualties.r), [_Some Weekend Reading_ blog]({{ site.baseurl }}/), 2023-May-09.  
+
+There is also [a textual transcript of running this]({{ site.baseurl }}/assets/2023-05-09-russian-casualty-update-update-ukr-rus-casualties-transcript.txt), so you can check that it says what I told you.  
+
+We've also archived
+[a .zip file of the original images uploaded by the Ukrainian MoD]({{ site.baseurl }}/assets/2023-05-09-russian-casualty-update-update-russian-casualty-data-images.zip),
+and [a .tsv format spreadsheet we constructed from that for analysis]({{ site.baseurl }}/assets/2023-05-09-russian-casualty-update-update-russian-casualties-in-ukraine.tsv).  
+
+__NB:__ There are also some subroutine files for graphics and analysis pipeline building
+(`graphics-tools.r` and `pipeline-tools.r`) that are loaded from our private repository.
+If you want those too for reproduction purposes, drop us an email and we'd be happy to send
+them along to you.  
+
+You might have to rename the script, create a data directory, and put the .tsv file in it with the appropriate name to make this work.  Ask if there's a problem.  Here at Ch&acirc;teau Weekend, we are peer-review-friendly. [↩](#fn3a)  
+
+<a id="fn4">4</a>: G Box, _et al.,_ ["Time Series Analysis: Forecasting and Control (5th ed)"](https://www.amazon.com/Time-Analysis-Forecasting-Probability-Statistics/dp/1118675029/), Wiley, 2015.  Mine is the older 3rd edition of 1994. [↩](#fn4a)  
+
+<a id="fn5">5</a>: R Hyndman &amp; G Athanasopoulos, ["Forecasting: Principles and Practice"](https://www.amazon.com/Forecasting-principles-practice-Rob-Hyndman/dp/0987507117/), [Open Access Textbooks](http://OTexts.com/fpp2/), 2018. [↩](#fn5a)  
---
+++ assets/2023-05-09-russian-casualty-update-update-ukr-rus-casualties.r
@@ -0,0 +1,263 @@
+## Created on Wednesday, April 12, 2023 at 1:52pm EDT by WeekendEditor on WeekendEditorMachine.
+## Copyright (c) 2023, SomeWeekendReading.blog.  All rights reserved.  As if you care.
+
+sourceTools <- function(lib) { source(file.path("~/Documents/laboratory/tools", lib)) }
+sourceTools("graphics-tools.r")                        # Miscellaneous graphics hacks
+sourceTools("pipeline-tools.r")                        # How to build analysis pipelines
+
+library("PerformanceAnalytics")                        # For chart.Correlation()
+
+##
+## Look at the Russian casualty data that the Ukrainian MoD has been posting to Twitter.
+##
+## I could only get Twitter to take me back as far as 2023-Jan-22.  I'm almost certain
+## they've been posting longer than that, but this is what Twitter is nowadays.
+##
+## NB: this is hypothesis FORMING research, not hypothesis TESTING.  We're just going to cast
+## around for pairwise significant associations, by Pearson correlation and by regression
+## (possibly transformed) with time.
+##
+## Some of it, frankly, is to look for artifacts that might indicate fudged data.  The incentives
+## are high, and the authorities who know better are secretive.  Hence, this crude effort.
+##
+
+doit <- function(## Inputs
+                 dataDir  = "./data",
+                 dataFile = "russian-casualties-in-ukraine.tsv",
+
+                 ## Outputs
+                 resultsDir  = "./results",
+                 script      = "ukr-rus-casualties.r",
+                 wabFile     = "warshipsAndBoats-useless.png",
+                 regressFile = "regress-%s-on-%s.png",
+                 biclFile    = "bicluster.png",
+                 corrFile    = "correlation-chart.png") {
+
+  loadAndQCData <- function(dataDir, dataFile) {       # Load the spreadsheet, check a few things
+
+    colIsNondecreasingPositiveIntegers <- function(df, colname) {
+      stopifnot(class(df[, colname]) == "integer" &&   # Are the elememnts of type integer?
+                all(df[, colname] > 0)            &&   # Are they all positive?
+                all(diff(df[, colname]) >= 0))         # Are they nondecreasing?
+    }                                                  #
+
+    cat(sprintf("* Loading and QCing data from spreadsheet"))
+
+    dataPath <- file.path(dataDir, dataFile)           # Announce data source, for checking we've
+    cat(sprintf("\n  - Input file: %s", dataPath))     #  got the right file on the transcript.
+
+    ukrRusCasualties <- transform(read.table(dataPath, header = TRUE, sep = "\t"),
+                                  Date = as.Date(as.character(Date), format = "%m/%d/%Y"))
+    cat(sprintf("\n  - Got a %d-row dataframe of %d columns: ",
+                nrow(ukrRusCasualties), ncol(ukrRusCasualties)))
+    cat(sprintf("\n    %s", paste(colnames(ukrRusCasualties), collapse = ", ")))
+
+    ukrRusCasualties <- subset(ukrRusCasualties, subset = complete.cases(ukrRusCasualties))
+    cat(sprintf("\n  - %d rows had complete data", nrow(ukrRusCasualties)))
+
+    cat(sprintf("\n  - Checking column types:"))       # Check types of columns
+    ## DayNum is an integer from 1 to the number of rows.
+    stopifnot(colnames(ukrRusCasualties)[[1]] == "DayNum"  &&
+              class(ukrRusCasualties[, 1])    == "integer")
+              ## Can't do this now that there are missing data rows
+              ## && all(ukrRusCasualties$"DayNum"   == 1 : nrow(ukrRusCasualties)))
+    ## Date is a sequence of Dates, ascending by exactly 1 day as we go to the next row.
+    stopifnot(colnames(ukrRusCasualties)[[2]] == "Date"    &&
+              class(ukrRusCasualties[, 2])    == "Date")
+              ## Can't do this now that there are missing data rows
+              ## && all(ukrRusCasualties$"Date"     == seq(from = ukrRusCasualties[[1, "Date"]],
+              ##                                       to   = ukrRusCasualties[[nrow(ukrRusCasualties),
+              ##                                                                "Date"]],
+              ##                                       by   = 1)))
+    cat(sprintf("\n    o DayNum and Date have appropriate sequence values"))
+
+    ## All other rows are non-negative integers, and behavior w/time is monotone nondecreasing.
+    ## (Soldiers are not, at present, resurrected from the dead.)
+    sapply(colnames(ukrRusCasualties)[-(1:2)], function(colname) {
+      colIsNondecreasingPositiveIntegers(ukrRusCasualties, colname)
+    })                                                 #
+    cat(sprintf("\n    o All other columns are nondecreasing positive integers\n"))
+
+    ukrRusCasualties                                   # Return the data in a dataframe
+  }                                                    #
+
+  wabUseless <- function(df, resultsDir, wabFile) {    #
+    f <- file.path(resultsDir, wabFile)                # Argument for removing WarshipsAndBoats
+    cat(sprintf("* Summarizing argument of uselessness of WarshipsAndBoats to %s.\n", f))
+    withPNG(f, 750, 375, FALSE, function() {           #
+      withPars(function() {                            #
+        plot(ukrRusCasualties$"DayNum", ukrRusCasualties$"WarshipsAndBoats",
+             pch = 21, bg = "blue", type = "b", lty = "dashed", col = "black",
+             xlab = "Day Number", ylab = "Warships/Boats",
+             main = "WarshipsAndBoats is Uninformative")
+      }, pty = "m",                                    #
+         bg  = "white",                                #
+         ps  = 16,                                     #
+         mar = c(3, 3, 2, 1),                          #
+         mgp = c(1.7, 0.5, 0.0))                       #
+    })                                                 #
+  }                                                    #
+
+  chartCorrelation <- function(mx, resultsDir, corrFile) {
+    f <- file.path(resultsDir, corrFile)               #
+    cat(sprintf("\n* Doing multivariate correlation chart to %s", f))
+    withPNG(f, 1500, 1500, FALSE, function() {         #
+      withPars(function() {                            # Gets some graphics param warnings?
+        chart.Correlation(R = mx, histogram = TRUE, method = "pearson", pch = 19)
+      }, pty = "m",                                    #
+         bg  = "white",                                #
+         ps  = 16,                                     #
+         mar = c(3, 3, 2, 1),                          #
+         mgp = c(1.7, 0.5, 0.0))                       #
+    })                                                 #
+    cat("\n")                                          #
+  }                                                    #
+
+  plotCol1VsCol2 <- function(df, col1, col2, resultsDir, regressFile) {
+    f <- file.path(resultsDir, sprintf(regressFile, col1, col2))
+    cat(sprintf("\n* Analyzing %s vs %s", col1, col2)) #
+    cat(sprintf("\n  - Regression plot to %s.", f))    #
+    withPNG(f, 750, 750, FALSE, function() {           #
+      withPars(function() {                            #
+        ## Scatterplot with rug but no contours, and with regression line + conf/pred intervals
+        mdl <- scatterplotWithDensities(xs = df[, col2], ys = df[, col1], bgs = "blue",
+                                        xlab = col2, ylab = col1, kdeN = 0,
+                                        main = sprintf("Russian Losses in Ukraine: %s vs %s",
+                                                       col1, col2),
+                                        regressionColor = "black")
+        print(summary(mdl))                            #
+        cat("\n")                                      #
+        mdl                                            # Return the model
+      }, pty = "m",                                    # Maximal plotting area
+         bg  = "white",                                # Silly bkgnd for web
+         ps  = 16,                                     # Larger type size for file capture
+         mar = c(3, 3, 2, 1),                          # Pull in on margins
+         mgp = c(1.7, 0.5, 0.0))                       # Axis title, labels, ticks
+    })                                                 # Restore params & end file capture
+  }                                                    #
+
+  correlationBicluster <- function(mx, resultsDir, biclFile, legendFrac = 0.10) {
+    f <- file.path(resultsDir, biclFile)               #
+    cat(sprintf("\n* Biclustering the correlation matrix to %s", f))
+    withPNG(f, 750, 750, FALSE, function() {           #
+      withPars(function() {                            #
+        corrMx    <- cor(mx)                           # Correlation matrix of various casualty types
+        corrRange <- range(corrMx)                     # Range of Pearson correlations.
+        colors    <- makeSaturableHeatmapColorsBWR(256,
+                                                   minObs = corrRange[[1]],
+                                                   maxObs = corrRange[[2]],
+                                                   minSat = corrRange[[1]]-0.01,
+                                                   midSat = corrRange[[1]],
+                                                   maxSat = corrRange[[2]])
+        heatmap(corrMx, scale = "none", symm = TRUE, margins = c(15, 15), col = colors,
+                main = "Russian Casualty Correlations", add.expr = box(which = "plot"))
+
+        par("omd" = c(0, 1, 0, legendFrac), "pty" = "m", "mar" = c(3, 1, 1, 1), "new" = TRUE)
+        maxx <- max(abs(corrRange))                    #
+        image(x    = seq(from = corrRange[[1]], to = corrRange[[2]], along.with = colors),
+              y    = 0,                                # Color bar legend: scale of enrichments
+              z    = matrix(1 : length(colors), nrow = length(colors)),
+              xlab = "Pearson Correlation", cex.axis = 0.8, mgp = c(1.7, 0.5, 0),
+              xlim = c(-maxx, +maxx),                  # Show range of POSSIBLE enrichments
+              col  = colors,                           # Same colors as in heatmap
+              yaxt = "n", ylab = NA)                   # Irrelevant y axis & label
+        ## abline(v = corrRange[[1]], lty = "solid", col= "black")# Show where colors
+        ## abline(v = corrRange[[2]], lty = "solid", col= "black")#  saturate
+        box(which = "plot")                            # Wrap it up in a pretty ribbon
+
+      }, pty = "m",                                    #
+         bg  = "white",                                #
+         ps  = 14,                                     #
+         omd = c(legendFrac, 0.99, legendFrac, 0.99),  # Main title somewhat cut off
+         mar = c(1, 1, 1, 1))                          #
+    })                                                 #
+  }                                                    #
+
+  predict200kDay <- function(ukrRusCasualties, nCasualties, resultsDir, regressFile) {
+    ## Predict day when casualties hit nCasualties (e.g., 200k)
+
+    ## This isn't sufficient: custom xlim, and vertical line @ nCasualties
+    ## plotCol1VsCol2(ukrRusCasualties, "DayNum", "Soldiers", resultsDir, regressFile)
+    f <- file.path(resultsDir, sprintf(regressFile, "DayNum", "Soldiers"))
+    withPNG(f, 750, 750, FALSE, function() {
+      withPars(function() {
+        scatterplotWithDensities(xs = ukrRusCasualties$"Soldiers",
+                                 ys = ukrRusCasualties$"DayNum",
+                                 bgs = "blue", xlab = "Soldiers", ylab = "DayNum",
+                                 kdeN = 0, main = "Russian Losses in Ukraine: DayNum vs Soldiers",
+                                 regressionColor = "black",
+                                 xlim = c(min(ukrRusCasualties$"Soldiers"),
+                                          max(max(ukrRusCasualties$"Soldiers"), nCasualties)))
+        abline(v = nCasualties, col = "gray", lty = "dashed")
+      }, pty = "m",
+         bg  = "white",
+         ps  = 16,
+         mar = c(3, 3, 2, 1),
+         mgp = c(1.7, 0.5, 0.0))})
+    ## Estimate DayNum when have nCasualties with regression of DayNum on Soldiers
+    mdl <- lm(DayNum ~ Soldiers, data = ukrRusCasualties)
+    print(summary(mdl))
+    print(transform(predict(mdl, newdata = data.frame("Soldiers" = nCasualties),
+                            interval = "prediction"),
+                    fit = fit + as.Date("2023-01-22"),
+                    lwr = lwr + as.Date("2023-01-22"),
+                    upr = upr + as.Date("2023-01-22")))
+  }
+
+  ## Main body of analysis script begins here
+  withTranscript(dataDir, resultsDir,                  # Capture stdout to transcript
+                 sprintf("%s-transcript.txt", sub("^(.*)\\.r$", "\\1", basename(script))),
+                 "Analysis of Russian Casualties in Ukraine", function() {
+
+    heraldPhase("Archival of script and data")         # Archive data & script for peer review
+    archiveAnalysisScript(resultsDir, file.path(dataDir, dataFile), file.path(".", script))
+
+    heraldPhase("Loading and QC of data")              # Load from the hand-built spreadsheet
+    maybeAssign("ukrRusCasualties", function() { loadAndQCData(dataDir, dataFile) })
+
+    heraldPhase("Analysis of data")                    # Main analysis
+    maybeAssign("analysisDone", function() {           #
+
+      wabUseless(ukrRusCasualties, resultsDir, wabFile)# Near-constant, changes only on day 2
+      mx <- as.matrix(subset(ukrRusCasualties, select = -c(DayNum, Date, WarshipsAndBoats)))
+      cat("\n* Range of correlations:\n"); print(range(cor(mx)))
+
+      ## Correlation and bicluster of reasonable columns
+      chartCorrelation(mx, resultsDir, corrFile)       # Look at all pairwise correlations
+      correlationBicluster(mx, resultsDir, biclFile)   # Correlation bicluster
+
+      ## Top group: CruiseMissiles, Helicopters, MilitaryJets.  Do all pairwise correlations + time.
+      mdl1 <<- plotCol1VsCol2(ukrRusCasualties, "CruiseMissiles", "Helicopters",
+                              resultsDir, regressFile)
+      mdl2 <<- plotCol1VsCol2(ukrRusCasualties, "CruiseMissiles", "MilitaryJets",
+                              resultsDir, regressFile)
+      mdl3 <<- plotCol1VsCol2(ukrRusCasualties, "Helicopters", "MilitaryJets",
+                              resultsDir, regressFile)
+      mdl4 <<- plotCol1VsCol2(ukrRusCasualties, "CruiseMissiles", "DayNum", resultsDir, regressFile)
+      mdl5 <<- plotCol1VsCol2(ukrRusCasualties, "MilitaryJets", "DayNum", resultsDir, regressFile)
+      mdl6 <<- plotCol1VsCol2(ukrRusCasualties, "Helicopters", "DayNum", resultsDir, regressFile)
+
+      ## 2nd, larger group: so highly correlated, just do vs time
+      mdl7 <<- plotCol1VsCol2(ukrRusCasualties, "Soldiers", "DayNum", resultsDir, regressFile)
+      mdl8 <<- plotCol1VsCol2(ukrRusCasualties, "Tanks", "DayNum", resultsDir, regressFile)
+      mdl9 <<- plotCol1VsCol2(ukrRusCasualties, "ArmoredCombatVehicles", "DayNum",
+                              resultsDir, regressFile)
+      mdl10 <<- plotCol1VsCol2(ukrRusCasualties, "Artillery", "DayNum", resultsDir, regressFile)
+      mdl11 <<- plotCol1VsCol2(ukrRusCasualties, "MultipleLaunchRocketSystems", "DayNum",
+                               resultsDir, regressFile)
+      mdl12 <<- plotCol1VsCol2(ukrRusCasualties, "AirDefenceSystems", "DayNum",
+                               resultsDir, regressFile)
+      mdl13 <<- plotCol1VsCol2(ukrRusCasualties, "Drones", "DayNum", resultsDir, regressFile)
+      mdl14 <<- plotCol1VsCol2(ukrRusCasualties, "VehiclesAndFuelTanks", "DayNum",
+                               resultsDir, regressFile)
+      mdl15 <<- plotCol1VsCol2(ukrRusCasualties, "SpecialEquipment", "DayNum",
+                               resultsDir, regressFile)
+
+      predict200kDay(ukrRusCasualties, 200000, resultsDir, regressFile)
+
+      cat("\n")                                        #
+      TRUE                                             # Flag that it's done
+    })                                                 # End of analysis
+
+  })                                                   # End transcript capture
+}                                                      #
